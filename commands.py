efa1c2a76fb9eaadb05d29b162d3a9dd5e4c4770
Jakub Jirutka <jakub@jirutka.cz>
2015-07-28 16:34:59 +0200

    Add PEP8 and setup travis

--- commands.py
+++ commands.py
@@ -1,8 +1,9 @@
 from sublime_plugin import TextCommand
 
+
 class ReplaceFollowingCharacterCommand(TextCommand):
     """ Replace the following character after the cursor with the replacement. """
 
     def run(self, edit, replacement=' '):
         self.view.run_command('right_delete')
-        self.view.run_command('insert', { 'characters': replacement })
+        self.view.run_command('insert', {'characters': replacement})
